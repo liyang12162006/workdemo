7f2b1c08459c111ae7dc97cc30e2c0a12b76fb0d
liyangyang <liyangyang@huitongjy.com>
2019-05-10 16:42:02 +0800

    init

--- src/main/java/com/example/workdemo/WorkdemoApplication.java
+++ src/main/java/com/example/workdemo/WorkdemoApplication.java
@@ -8,6 +8,8 @@ public class WorkdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkdemoApplication.class, args);
+
+        System.out.println("启动成功了");
     }
 
 }
